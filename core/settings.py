3b2ad56c4dba148fe7add45213b98038b2d4353c
Kwame Domfe Appau <nkadbasics@gmail.com>
2026-05-09 06:27:13 +0000

    Initial commit

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-CHANGE_THIS_TO
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'celebs-africa-o3zfi.ondigitalocean.app').split(',')
 
 
 # Application definition
