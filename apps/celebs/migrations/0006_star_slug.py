82ca4831ec0abbbdd7fa3c502b91d55eec1c1be8
Kwame Domfe Appau <nkadbasics@gmail.com>
2026-05-09 07:31:45 +0000

    Initial commit

--- apps/celebs/migrations/0006_star_slug.py
+++ apps/celebs/migrations/0006_star_slug.py
@@ -5,7 +5,7 @@ from django.utils.text import slugify
 
 
 def populate_slugs(apps, schema_editor):
-    Star = apps.get_model('celebs', 'Celebs')
+    Star = apps.get_model('celebs', 'Star')
     for star in Star.objects.all():
         base_slug = slugify(star.name) or f"star-{star.pk}"
         slug = base_slug
