96a8df94aa080442be060de4d1ecc7068c8de283
Kwame Domfe Appau <nkadbasics@gmail.com>
2026-05-09 08:07:38 +0000

    Reset Database

--- apps/celebs/migrations/0007_rename_star_to_celeb.py
+++ apps/celebs/migrations/0007_rename_star_to_celeb.py
@@ -3,6 +3,33 @@ from django.conf import settings
 from django.db import migrations, models
 
 
+def rename_tables(apps, schema_editor):
+    """
+    Handles both old databases (tables named stars_*) and fresh databases
+    (tables already named celebs_* because the app was always called celebs).
+    """
+    with schema_editor.connection.cursor() as cursor:
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+        tables = {row[0] for row in cursor.fetchall()}
+
+        if 'stars_star' in tables:
+            cursor.execute('ALTER TABLE stars_star RENAME TO celebs_celeb')
+        elif 'celebs_star' in tables:
+            cursor.execute('ALTER TABLE celebs_star RENAME TO celebs_celeb')
+
+        if 'stars_like' in tables:
+            cursor.execute('ALTER TABLE stars_like RENAME TO celebs_like')
+
+        if 'stars_comment' in tables:
+            cursor.execute('ALTER TABLE stars_comment RENAME TO celebs_comment')
+
+        if 'celebs_like' in tables or 'stars_like' in tables:
+            cursor.execute('ALTER TABLE celebs_like RENAME COLUMN star_id TO celeb_id')
+
+        if 'celebs_comment' in tables or 'stars_comment' in tables:
+            cursor.execute('ALTER TABLE celebs_comment RENAME COLUMN star_id TO celeb_id')
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -13,11 +40,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.SeparateDatabaseAndState(
             database_operations=[
-                migrations.RunSQL('ALTER TABLE stars_star RENAME TO celebs_celeb'),
-                migrations.RunSQL('ALTER TABLE stars_like RENAME TO celebs_like'),
-                migrations.RunSQL('ALTER TABLE stars_comment RENAME TO celebs_comment'),
-                migrations.RunSQL('ALTER TABLE celebs_like RENAME COLUMN star_id TO celeb_id'),
-                migrations.RunSQL('ALTER TABLE celebs_comment RENAME COLUMN star_id TO celeb_id'),
+                migrations.RunPython(rename_tables, migrations.RunPython.noop),
             ],
             state_operations=[
                 migrations.RenameModel('Star', 'Celeb'),
