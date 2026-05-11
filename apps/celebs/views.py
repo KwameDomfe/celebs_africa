e44cc08a6d68976844db94e7c0f19092fd35a074
Kwame Domfe Appau <nkadbasics@gmail.com>
2026-05-11 11:24:02 +0000

    Loaded countries and flags

--- apps/celebs/views.py
+++ apps/celebs/views.py
@@ -20,6 +20,7 @@ def staff_required(view_func):
 
 def celebs_home(request):
 	categories = Category.objects.prefetch_related('families__types').order_by('name')
+	countries = Country.objects.all()
 	celebs = Celeb.objects.select_related('type__family__category').annotate(
 		follower_count=Count('followers'),
 		avg_rating=Avg('reviews__rating'),
@@ -29,6 +30,7 @@ def celebs_home(request):
 	cat_id = request.GET.get('category', '')
 	family_id = request.GET.get('family', '')
 	type_id = request.GET.get('type', '')
+	country_id = request.GET.get('country', '')
 	q = request.GET.get('q', '')
 	q_filter = q.strip()
 	if cat_id:
@@ -37,14 +39,18 @@ def celebs_home(request):
 		celebs = celebs.filter(type__family_id=family_id)
 	if type_id:
 		celebs = celebs.filter(type_id=type_id)
+	if country_id:
+		celebs = celebs.filter(nationality_id=country_id)
 	if q_filter:
 		celebs = celebs.filter(name__icontains=q_filter) | celebs.filter(street_name__icontains=q_filter)
 	return render(request, 'celebs/celeb_list.html', {
 		'celebs': celebs,
 		'categories': categories,
+		'countries': countries,
 		'selected_category': cat_id,
 		'selected_family': family_id,
 		'selected_type': type_id,
+		'selected_country': country_id,
 		'q': q,
 	})
 
