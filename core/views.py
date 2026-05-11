bba761e141cae602122ea615e75fbcd95ab40c8e
Kwame Domfe Appau <nkadbasics@gmail.com>
2026-05-11 12:55:25 +0000

    Updated the hom page

--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
-from apps.celebs.models import Celeb, Category
+from apps.celebs.models import Celeb, Category, Country
 from django.shortcuts import render
 from django.db.models import Count, Avg
 
 def home(request):
     categories = Category.objects.prefetch_related('families__types').order_by('name')
+    countries = Country.objects.all()
     celebs = Celeb.objects.select_related('type__family__category', 'nationality').annotate(
         follower_count=Count('followers'),
         avg_rating=Avg('reviews__rating'),
@@ -13,6 +14,7 @@ def home(request):
     cat_id = request.GET.get('category', '')
     family_id = request.GET.get('family', '')
     type_id = request.GET.get('type', '')
+    country_id = request.GET.get('country', '')
     q = request.GET.get('q', '')
     q_filter = q.strip()
     if cat_id:
@@ -21,14 +23,18 @@ def home(request):
         celebs = celebs.filter(type__family_id=family_id)
     if type_id:
         celebs = celebs.filter(type_id=type_id)
+    if country_id:
+        celebs = celebs.filter(nationality_id=country_id)
     if q_filter:
         celebs = celebs.filter(name__icontains=q_filter) | celebs.filter(street_name__icontains=q_filter)
     context = {
         'celebs': celebs,
         'categories': categories,
+        'countries': countries,
         'selected_category': cat_id,
         'selected_family': family_id,
         'selected_type': type_id,
+        'selected_country': country_id,
         'q': q,
     }
     return render(request, 'home.html', context)
